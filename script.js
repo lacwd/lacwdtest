1f2f9dc8b7bfa34c728d314746703f1425035368
Reggie Cart <wportil@wgu.edu>
2025-09-06 15:51:42 -0700

    fixing

--- script.js
+++ script.js
@@ -57,7 +57,8 @@ for (const item of tertiaryDrop) {
     item.classList.toggle("drop3-active");
   });
 }
-// FAQ
+
+// pricing
 const faqItems = Array.from(document.querySelectorAll(".cs-faq-item"));
 for (const item of faqItems) {
   const onClick = () => {
@@ -65,45 +66,3 @@ for (const item of faqItems) {
   };
   item.addEventListener("click", onClick);
 }
-
-class FAQFilter {
-  filtersSelector = ".cs-option";
-  FAQselector = ".cs-faq-group";
-  activeClass = "cs-active";
-  hiddenClass = "cs-hidden";
-
-  constructor() {
-    const $filters = document.querySelectorAll(this.filtersSelector);
-    this.$activeFilter = $filters[0];
-    this.$images = document.querySelectorAll(this.FAQselector);
-
-    this.$activeFilter.classList.add(this.activeClass);
-
-    for (const $filter of $filters) {
-      $filter.addEventListener("click", () => this.onClick($filter));
-    }
-  }
-
-  onClick($filter) {
-    this.filter($filter.dataset.filter);
-
-    const { activeClass } = this;
-
-    this.$activeFilter.classList.remove(activeClass);
-    $filter.classList.add(activeClass);
-
-    this.$activeFilter = $filter;
-  }
-
-  filter(filter) {
-    const showAll = filter == "all";
-    const { hiddenClass } = this;
-
-    for (const $image of this.$images) {
-      const show = showAll || $image.dataset.category == filter;
-      $image.classList.toggle(hiddenClass, !show);
-    }
-  }
-}
-
-new FAQFilter();
